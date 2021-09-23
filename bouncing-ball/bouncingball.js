2100f1ea8f813f09b5c55d30cc1f91913e592e8f
Malthe H Andersen <malthehandersen@Glenn-MacBook-Air.local>
2021-09-23 10:14:52 +0200

    smile

--- bouncing-ball/bouncingball.js
+++ bouncing-ball/bouncingball.js
@@ -1,10 +1,15 @@
 //variables
-var canvasX = innerWidth / 2;
-var canvasY = innerHeight / 2.5;
+var canvasX = innerWidth;
+var canvasY = innerHeight;
 var diameter = 50;
-var ballX = 400;
-var ballY = 200;
-var speed = 2;
+var ballX = 980;
+var ballY = 540;
+var xspeed = 2;
+var yspeed= 2;
+var pastRightEdge = canvasX < ballX + diameter;
+var pastLeftEdge = 0 > ballX - diameter;
+var belowBottom = canvasY < ballY + diameter;
+var aboveTop = 0 > ballY + diameter;
 
 //start of preload
 function preload() {
@@ -14,8 +19,8 @@ function preload() {
 
 //start of responsiveness
 function windowResized() {
-    canvasX = innerWidth / 2;
-    canvasY = innerHeight / 2.5;
+    canvasX = innerWidth;
+    canvasY = innerHeight;
     var Canvas = createCanvas(canvasX, canvasY);
     Canvas.parent('bouncingballDiv');
 }
@@ -30,16 +35,26 @@ function setup() {
 
 //start of draw 
 function draw() {
+    ballX = ballX + xspeed
+    ballY = ballY + yspeed
+    if(pastLeftEdge || pastRightEdge){
+    xspeed = -xspeed
+   }
+    if(aboveTop || belowBottom){
+    yspeed = -yspeed
+   }
+
     if(keyIsDown(81) || keyIsDown(81)){
-        diameter -= 1;
+        diameter -= 2;
     }
     if(keyIsDown(69) || keyIsDown(69)){
-        diameter += 1;
+        diameter += 2;
     }
+    
     pop()
     clear()
     fill(255,0,0)
-    circle(ballX,ballY,diameter,diameter)
+    circle(ballX,ballY,diameter)
     push()
     
 }
