dc9950c05ed37825d07006f89b8ca4c3e492d1b7
ioya666 <malthe02dk@gmail.com>
2021-11-10 23:47:19 +0100

    asdads dsadsadwasadsdasdas

--- tasks/drawing/drawing.js
+++ tasks/drawing/drawing.js
@@ -1,54 +1,56 @@
-var startpos = ["",""];
-var endpos = ["",""];
-
 var startBool = false;
 var endBool = false;
-
-//start of responsiveness
+var linesarr = [];
+var lines;
 function windowResized() {
     canvasX = innerWidth / 2
     canvasY = innerHeight / 2.5
     var Canvas = createCanvas(canvasX, canvasY);
-    Canvas.parent('drawingDiv');
+    Canvas.parent('drawingDiv')
 }
-//end of responsiveness
-
-// start of setup
 function setup() {
     canvasX = innerWidth / 2
     canvasY = innerHeight / 2.5
     var Canvas = createCanvas(canvasX, canvasY);
     Canvas.parent('drawingDiv');
+    lines = new Lines(-5,-5,-5,-5);
+    linesarr.push(lines);
 }
-// end of setup
-
-//start of draw 
+function reset(){
+    linesarr = [];
+    lines = new Lines(-5,-5,-5,-5);
+    linesarr.push(lines);
+}
 function draw(){
-    background(0)
     stroke(0,255,0)
     strokeWeight(5)
-    line(startpos[0], startpos[1], endpos[0],endpos[1])
+    background(0)
+    keyIsDown(69)?(reset()):0;
 
-    if(mouseIsPressed){
-        if(endBool === false){
-            endpos[0] = mouseX;
-            endpos[1] = mouseY;            
-        }
-        if(startBool === false){
-            startpos[0] = mouseX;
-            startpos[1] = mouseY;
-            line(startpos[0],startpos[1],endpos[0],endpos[1])
-            startBool = true;
-        }
-    }  
-}
-//end of draw
-function mouseReleased(){
-    if(endBool === false){
-        endpos[0] = mouseX;
-        endpos[1] = mouseY;
-        endBool = true;
+    for(var i=0;i<linesarr.length;i++){
+        linesarr[i].draw();
     }
+    if(mouseIsPressed){
+        startBool == false?(linesarr[linesarr.length-1].startpos1=mouseX,linesarr[linesarr.length-1].startpos2=mouseY,startBool=true):0;
+        endBool == false?(linesarr[linesarr.length-1].endpos1=mouseX,linesarr[linesarr.length-1].endpos2=mouseY):0;
+    }
+    
+}
+function mouseReleased(){
+    endBool = true;
+    lines = new Lines(-5, -5, -5, -5);
+    linesarr.push(lines);
     startBool = false;
     endBool = false;
+}
+class Lines {
+    constructor(s1_,s2_,e1_,e2_){
+    this.startpos1 = s1_;
+    this.startpos2 = s2_;
+    this.endpos1 = e1_;
+    this.endpos2 = e2_;
+    }
+    draw(){
+        line(this.startpos1,this.startpos2,this.endpos1,this.endpos2);
+    }
 }
