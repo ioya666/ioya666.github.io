d7407779e34e4b2e1fd92ec081df561c4b73c72e
Malthe H Andersen <malthehandersen@Glenn-MacBook-Air.local>
2021-11-11 10:11:07 +0100

    llasdldasldasldaslkdasklsadlkdas

--- tasks/bouncing-ball/bouncingball.js
+++ tasks/bouncing-ball/bouncingball.js
@@ -1,4 +1,3 @@
-//variables
 var canvasX = innerWidth / 2;
 var canvasY = innerHeight / 2.5;
 var radius = 25
@@ -6,70 +5,34 @@ var ballX = canvasX / 2;
 var ballY = canvasY / 2;
 var xSpeed = 3;
 var ySpeed = 3;
-var resizeSpeed = 5/2
-
-//start of preload
+var resizeSpeed = 5 / 2
 function preload() {
     img = loadImage('../../images/bouncingballbackground.jpg')
 }
-//end of preload
-
-//start of responsiveness
 function windowResized() {
     canvasX = innerWidth / 2;
     canvasY = innerHeight / 2.5;
     var Canvas = createCanvas(canvasX, canvasY);
     Canvas.parent('bouncingballDiv');
 }
-//end of responsiveness
-
-// start of setup
 function setup() {
     var Canvas = createCanvas(canvasX, canvasY);
     Canvas.parent('bouncingballDiv');
 }
-// end of setup
-
-//start of draw 
 function draw() {
     background(img)
-
     if (keyIsDown(81)) {
-        if (radius > 5) {
-            radius -= resizeSpeed;
-        }
+        radius > 5 ? (radius -= resizeSpeed) : 0;
     }
-
     if (keyIsDown(69)) {
-        if (radius < 100) {
-            radius += resizeSpeed;
-        }
+        radius < 100 ? (radius += resizeSpeed) : 0;
     }
-   
     ballX = ballX + xSpeed;
     ballY = ballY + ySpeed;
-
     fill(255, 0, 0);
-    circle(ballX, ballY, radius*2);
-
-    if (canvasX < ballX + radius) {
-        ballX = canvasX - radius;
-        xSpeed = -xSpeed;
-    }
-    if (0 > ballX - radius) {
-        ballX = 0 + radius;
-        xSpeed = -xSpeed;
-    }
-
-    if (canvasY < ballY + radius) {
-        ballY = canvasY - radius;
-        ySpeed = -ySpeed;
-    }
-    if (0 > ballY - radius) {
-        ballY = 0 + radius;
-        ySpeed = -ySpeed;
-    }
-    
+    circle(ballX, ballY, radius * 2);
+    canvasX < ballX + radius ? (ballX = canvasX - radius, xSpeed = -xSpeed) : 0;
+    0 > ballX - radius ? (ballX = 0 + radius, xSpeed = -xSpeed) : 0;
+    canvasY < ballY + radius ? (ballY = canvasY - radius, ySpeed = -ySpeed) : 0;
+    0 > ballY - radius ? (ballY = 0 + radius, ySpeed = -ySpeed) : 0;
 }
-
-//end of draw
--- tasks/drawing/drawing.js
+++ tasks/drawing/drawing.js
@@ -1,42 +1,48 @@
-var startBool = false;
-var endBool = false;
-var linesarr = [];
-var lines;
-function windowResized() {
+let startBool = false;
+let endBool = false;
+let linesarr = [];
+
+function initCanvas() {
     canvasX = innerWidth / 2
     canvasY = innerHeight / 2.5
-    var Canvas = createCanvas(canvasX, canvasY);
-    Canvas.parent('drawingDiv')
+    let Canvas = createCanvas(canvasX, canvasY);
+    Canvas.parent('drawingDiv');
+}
+function windowResized() {
+    initCanvas()
 }
 function setup() {
-    canvasX = innerWidth / 2
-    canvasY = innerHeight / 2.5
-    var Canvas = createCanvas(canvasX, canvasY);
-    Canvas.parent('drawingDiv');
-    lines = new Lines(-5,-5,-5,-5);
-    linesarr.push(lines);
+    initCanvas()
+    reset()
 }
-function reset(){
+function reset() {
     linesarr = [];
-    lines = new Lines(-5,-5,-5,-5);
+    let lines = new Lines(-5, -5, -5, -5);
     linesarr.push(lines);
 }
-function draw(){
-    stroke(0,255,0)
+function draw() {
+    stroke(0, 255, 0)
     strokeWeight(5)
     background(0)
-    keyIsDown(69)?(reset()):0;
+    keyIsDown(69) ? (reset()) : 0;
 
-    for(var i=0;i<linesarr.length;i++){
+    for (let i = 0; i < linesarr.length; i++) {
         linesarr[i].draw();
     }
-    if(mouseIsPressed){
-        startBool == false?(linesarr[linesarr.length-1].startpos1=mouseX,linesarr[linesarr.length-1].startpos2=mouseY,startBool=true):0;
-        endBool == false?(linesarr[linesarr.length-1].endpos1=mouseX,linesarr[linesarr.length-1].endpos2=mouseY):0;
+    if (mouseIsPressed) {
+        if (startBool == false) {
+            linesarr[linesarr.length - 1].startpos1 = mouseX;
+            linesarr[linesarr.length - 1].startpos2 = mouseY;
+            startBool = true;
+        }
+        if (endBool == false) {
+            linesarr[linesarr.length - 1].endpos1 = mouseX
+            linesarr[linesarr.length - 1].endpos2 = mouseY
+        }
     }
-    
+
 }
-function mouseReleased(){
+function mouseReleased() {
     endBool = true;
     lines = new Lines(-5, -5, -5, -5);
     linesarr.push(lines);
@@ -44,13 +50,13 @@ function mouseReleased(){
     endBool = false;
 }
 class Lines {
-    constructor(s1_,s2_,e1_,e2_){
-    this.startpos1 = s1_;
-    this.startpos2 = s2_;
-    this.endpos1 = e1_;
-    this.endpos2 = e2_;
+    constructor(s1_, s2_, e1_, e2_) {
+        this.startpos1 = s1_;
+        this.startpos2 = s2_;
+        this.endpos1 = e1_;
+        this.endpos2 = e2_;
     }
-    draw(){
-        line(this.startpos1,this.startpos2,this.endpos1,this.endpos2);
+    draw() {
+        line(this.startpos1, this.startpos2, this.endpos1, this.endpos2);
     }
 }
