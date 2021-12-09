ddb3db013dac576bba6a517cb2d0d88c06509c5a
Malthe H Andersen <malthehandersen@Glenn-MacBook-Air.local>
2021-12-09 09:33:33 +0100

    asdasda

---
+++ adventkalender/day.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const inputFileName = ("day1.txt");
+let content = fs.readFileSync(inputFileName).toString();
+let depth = content.split("\n").map(1 => 1.trim())
+
+for (let i = 1; depth.length; i++){
+
+}
